3f0837f1bf14d70aea0f00913eb42163ff2f92ac
Reynell Bobihis <reynellbobihis@gmail.com>
2023-05-28 20:55:21 +0800

    Initial Commit

---
+++ KuryenteLoad/KuryenteLoadApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuryenteLoadApp.swift
+//  KuryenteLoad
+//
+//  Created by Reynell Bobihis on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct KuryenteLoadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
